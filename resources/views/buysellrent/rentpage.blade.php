95b58b4588687637d242457138e94cd9945863cb
imat19 <vergara.reymart82@gmail.com>
2023-02-07 19:33:51 +0800

    components

--- resources/views/buysellrent/rentpage.blade.php
+++ resources/views/buysellrent/rentpage.blade.php
@@ -17,52 +17,22 @@
 <body>
     {{-- this is navbar form components folder --}}
     @include('components.navbar')
-
     <div class="Bsr-banner-rent">
-        <div class='banner-title position-absolute top-50 start-50 translate-middle fw-bolder'> RENT</div>
+        <div class='banner-title position-absolute top-50 start-50 translate-middle fw-bolder'> Rent</div>
     </div>
     <div class="container-lg">
-        <div class="row">
-            <div class="col-12">
-                <div class="input-group mb-3 p-2 mt-3">
-                    <button class="btn btn-light px-3 dropdown-toggle border border-2" data-bs-toggle="dropdown"
-                        id="selecStatus">Select Status
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <a href="{{ route('buypage') }}" class="dropdown-item">BUY</a>
-                        <a href="{{ route('sellpage') }}" class="dropdown-item">SELL</a>
-                        <a href="{{ route('rentpage') }}" class="dropdown-item">RENT</a>
-                    </ul>
-                    <button class="btn btn-light px-3 border border-2 dropdown-toggle" type="button"
-                        id="baths ">Baths</button>
-                    <button class="btn btn-light px-3 border border-2 dropdown-toggle" type="button"
-                        id="button-addon2 ">Beds</button>
-                    <input type="text" class="form-control py-2 bg-light" placeholder="Search..."
-                        id="input-search" />
-                    <button class="btn btn-light px-5" type="button" id="search">Search</button>
-                </div>
-            </div>
-        </div>
-        <div class="d-flex justify-content-end">
-            <button type="button" class="btn btn-light dropdown-toggle " data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Default Order
-            </button>
-            <ul class="shadow dropdown-menu dropdown-menu-end ">
-                <li><button class="dropdown-item" type="button">Price(Low to High)</button></li>
-                <li><button class="dropdown-item" type="button">Price(High to Low)</button></li>
-                <li><button class="dropdown-item" type="button">Date New to Old</button></li>
-                <li><button class="dropdown-item" type="button">Date Old to Knew</button></li>
-            </ul>
-        </div>
+        @include('components.bsrSearch')
+        @include('components.bsrFilter')
+      
+        {{-- Card section --}}
         <div class="row row-cols-1 row-cols-md-3 g-3 mt-1">
             <div class="col-md-6 col-lg-4">
                 <div class="card h-100 shadow border border-0">
                     <div class="img-wrapper">
                         <img href="#" class="bsr-pict card-img-top"
-                            src="https://www.vistaresidences.com.ph/img/containers/main/img/minimalist-condo-in-manila.png/15ed8280fc19482cae110268a2faebf0.png"
+                            src='https://www.vistaresidences.com.ph/img/containers/main/img/minimalist-condo-in-manila.png/15ed8280fc19482cae110268a2faebf0.png'
                             alt="..."></img>
-                        <a href="#" class='fade content stretched-link text-white'>
+                        <a href="#" class="fade content stretched-link text-white">
                             <img class='logo-overlay'
                                 src='https://i.ibb.co/H75pK9V/Dream-Fields-Logo-White-01.png'></img>
                             <span class='mt-3'> View Details</span></a>
@@ -109,9 +79,9 @@
             </div>
         </div>
     </div>
-
-     {{-- this is footer form components folder--}}  
-@include('components.footer')
+    
+ {{-- this is footer form components folder --}}
+ @include('components.footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
