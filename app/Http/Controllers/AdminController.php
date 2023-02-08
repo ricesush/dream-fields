74584764e249cac1450ab3956a852da2ea6bd07d
AloSamaa <kaizkiegt@gmail.com>
2023-02-08 20:06:01 +0800

    a

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -24,6 +24,6 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin')->with('properties', Property::orderBy('created_at', 'desc')->paginate(6));
+        return view('admin')->with('properties', Property::orderBy('created_at', 'desc')->get());
     }
 }
