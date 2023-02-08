c43f20e5e9653d95c809273a153ecc70147e9271
AloSamaa <kaizkiegt@gmail.com>
2023-02-08 12:20:44 +0800

    dashboard2

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,10 +1,160 @@
 @extends('layouts.adminLayout')
 
 @section('content')
-<div class="container">
-    <div class="h4 pt-3 ps-5">
+<div class="container1">
+    <div class="h4 pt-3 ps-5 mb-5">
         Welcome! <span class="fw-bold">{{Auth::user()->name}}</span>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body shadow-lg">
+                        <div class="h4 pt-2 text-center"><strong>Listing Requests</strong></div>
+                        <hr>
+                        <div class="tabcard">
+                        <table class="table table-hover text-center">
+                            <thead>
+                              <tr>
+                                <th>Unit Number</th>
+                                <th>Unit Type</th>
+                                <th>Unit Owner</th>
+                                <th>Lsting Type</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>3</td>
+                                <td>Larry the Bird</td>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>4</td>
+                                <td>Larry the Bird</td>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>5</td>
+                                <td>Larry the Bird</td>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>5</td>
+                                <td>Larry the Bird</td>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>5</td>
+                                <td>Larry the Bird</td>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@mdo</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                    </div>
+                </div>         
+            </div>
+
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body shadow-lg">
+                        <div class="h4 pt-2 text-center"><strong>Listed Units</strong></div>
+                        <hr>
+                        <div class="tabcard">
+                        <table class="table table-hover text-center">
+                            <thead>
+                              <tr>
+                                <th>Unit Number</th>
+                                <th>Unit Type</th>
+                                <th>Unit Owner</th>
+                                <th>Lsting Type</th>
+                                <th>Action</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>3</td>
+                                <td>Larry the Bird</td>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>4</td>
+                                <td>Larry the Bird</td>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>5</td>
+                                <td>Larry the Bird</td>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>5</td>
+                                <td>Larry the Bird</td>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <td>5</td>
+                                <td>Larry the Bird</td>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@mdo</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                    </div>
+                </div>               
+        </div>
+    </div>
+    <div class="container">
     <div class="row justify-content-center">
         <div class="col-12 col-md-3 mt-2">
             <div class="card p-3 mt-5 shadow-lg">
@@ -49,6 +199,7 @@
             </div>
         </div>
     </div>
+</div>
 </div>
     </div>
 </div>
