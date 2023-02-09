4f97de0e89901d0be152ec29b42367444331aa87
AloSamaa <kaizkiegt@gmail.com>
2023-02-09 10:11:59 +0800

    a

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -115,7 +115,7 @@
         @yield('header')
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group me-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+            <a href={{route('welcome')}} type="button" class="btn btn-sm btn-outline-secondary">back</a>
             <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
           </div>
           <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
@@ -126,7 +126,7 @@
       </div>
  
 
-        <main class="py-4">
+        <main class="py-4 container">
             @yield('content')
         </main>
         </main>
