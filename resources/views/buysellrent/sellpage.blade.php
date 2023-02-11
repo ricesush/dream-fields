71429ae598edbb31a572a54be5f6926cd8963257
imat19 <vergara.reymart82@gmail.com>
2023-02-11 19:08:45 +0800

    added  login

--- resources/views/buysellrent/sellpage.blade.php
+++ resources/views/buysellrent/sellpage.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.buysellrent')
-
+@include('auth.login')
 @section('content')
+<div class="container-info">
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-md-6">
+                @include('auth.login')
+            </div>
+            
+        </div>
+    </div>
+</div>
 
 
 
