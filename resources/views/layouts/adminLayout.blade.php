ebcf66989ca05b3ea7062c54c0d49d34570d66d6
AloSamaa <kaizkiegt@gmail.com>
2023-02-12 17:01:16 +0800

    fixed logout button

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -72,7 +72,6 @@
 
       <a href="{{ route('logout') }}"  onclick="event.preventDefault();
         document.getElementById('logout-form').submit();">
-     </a>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
           @csrf
       </form>
