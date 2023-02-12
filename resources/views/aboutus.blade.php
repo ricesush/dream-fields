deab0f8d5786eaa8df31df6c17c369ec9f706bd7
imat19 <vergara.reymart82@gmail.com>
2023-02-13 02:25:10 +0800

    added aboutpage

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,7 +1,94 @@
-@extends('layouts.guest')
+@extends('layouts.buysellrent')
 
 @section('content')
-<div class="container text-light">
-    About Us goes here!
-</div>
+    <div class="container-lg text-light mb-5 pt-5 text-center">
+        <img class="img-fluid" src="..\images\DreamFields Logo(full)(green)-01.png" alt="" width="500px">
+    </div>
+    <div class="container-lg">
+        <div class="row">
+            <div class="col mb-5">
+                <h1 class="fw-bold"><span class="border-bottom border-5"">About Us</span></h1>
+               
+                <p>Welcome to <span class="fw-bold"> Dream Fields </span>, your premier source for all things real estate.
+                    Our team
+                    of experienced and
+                    knowledgeable real estate professionals is dedicated to providing our clients with the highest level of
+                    service
+                    and expertise in the industry.
+                </p>
+                <p>
+                    We understand that buying, renting or selling a Condo Unit is a big decision, and we strive to make the
+                    process
+                    as seamless and
+                    stress-free as possible. Our team is equipped with the latest technology and market knowledge to ensure
+                    that our
+                    clients receive the best possible outcome.
+                </p>
+                <p>
+                    If you're looking for a team of dedicated and knowledgeable real estate professionals, look no further
+                    than
+                    <span class="fw-bold">Dream Fields</span> . Contact us today to learn more about how we can help you
+                    with all of
+                    your real estate needs.
+                </p>
+            </div>
+
+        </div>
+        <div class="row g-3 mb-3">
+            <div class="col-12 col-md-6 align-self-center">
+                <img class="img-fluid mx-auto d-block" style="max-width: 70%;" src="../images/about/Missions.png" alt="">
+            </div>
+            <div class="col-12 col-md-6 align-self-center">
+                <div>
+                    <h1 class="fw-bold"><span class="border-bottom border-5">Mission</span></h1>
+                    
+                    <p>Our mission at <span class="fw-bold"> Dream Fields </span> is to provide our clients with the highest
+                        level of service and expertise in the
+                        real estate industry. We are committed to helping our clients achieve their real estate goals by
+                        leveraging our
+                        knowledge, technology, and market insights. We believe that communication, transparency, and a
+                        client-first
+                        approach are key to our success and to building long-lasting relationships with our clients.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row g-3 swap-column mb-3">
+            <div class="col-12 col-md-6 align-self-center">
+                <h1 class="fw-bold"><span class="border-bottom border-5"">Vision</span></h1>
+                
+                <p>Our vision at <span class="fw-bold"> Dream Fields </span> is to be the leading real estate company in
+                    <span class="fw-bold"> Philippines</span> and beyond, known for
+                    providing exceptional service and delivering outstanding results for our clients. We strive to be a
+                    trusted
+                    partner for all of our clients' real estate needs and to consistently exceed their expectations.
+                </p>
+            </div>
+            <div class="col-12 col-md-6 align-self-center">
+                <img class="img-fluid mx-auto d-block" style="max-width: 70%;" src="../images/about/Visions.png" alt="">
+            </div>
+        </div>
+        <div class="row g-3 mb-3">
+            <div class="col-12 col-md-6 align-self-center">
+                <img class="img-fluid mx-auto d-block" style="max-width: 70%;" src="../images/about/value.png" alt="">
+            </div>
+            <div class="col-12 col-md-6 align-self-center">
+                <h1 class="fw-bold"><span class="border-bottom border-5"">Values</span></h1>
+                
+                <p>Customer Focus: Putting the needs and interests of our clients first and delivering exceptional service
+                    at all
+                    times.</p>
+                <p>Integrity: Conducting ourselves with honesty and transparency in all of our dealings.</p>
+                <p>Innovation: Embracing technology and new ideas to improve our services and stay ahead of the competition.
+                </p>
+                <p>Teamwork: Fostering a supportive and inclusive workplace culture where everyone works together towards a
+                    common
+                    goal.</p>
+                <p>Professionalism: Maintaining the highest standards of conduct and expertise in the real estate industry.
+                </p>
+
+            </div>
+        </div>
+
+    </div>
 @endsection
