211863372d9b65b40f60da93c59164b17adbd2e7
ricesush <pixelartme@gmail.com>
2023-02-15 03:04:14 +0800

    Update

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -6,10 +6,10 @@
     <div class="container-lg">
         <div class="row">
             <div class="col mb-5">
-                <h1 class="fw-bold mb-5"><span class="border-bottom border-5"">About Us</span></h1>
+                <h1 class="fw-bold mb-5 text-center"><span class="border-bottom border-b ">About Us</span></h1>
 
                 <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                    Welcome to <span class="fw-bold"> Dream Fields </span>,
+                    Welcome to <span class="fw-bold"> Dream <span class="orangeText">Fields</span> </span>,
                     your premier source for all things real estate. Our teamc of experienced and
                     knowledgeable real estate professionals is dedicated to providing our clients with the highest level of
                     service and expertise in the industry.
@@ -22,7 +22,7 @@
                 <p>
                     If you're looking for a team of dedicated and knowledgeable real estate professionals, look no further
                     than
-                    <span class="fw-bold">Dream Fields</span> . Contact us today to learn more about how we can help you
+                    <span class="fw-bold">Dream <span class="orangeText">Fields</span></span> . Contact us today to learn more about how we can help you
                     with all of
                     your real estate needs.
                 </p>
@@ -30,19 +30,19 @@
 
         </div>
         <div class="row g-1 mb-3">
-            <div class="col-12 col-md-6 align-self-center  pb-3" data-aos="fade-up-right">
+            <div class="col-12 col-md-6 align-self-center pb-3" data-aos="fade-up-right">
                 <img class="img-fluid mx-auto d-block shadow" style="max-width: 100%;"
                     src="https://media.istockphoto.com/id/1341791674/photo/businessman-working-on-laptop-computer-with-electronics-document-icons-e-document-management.jpg?b=1&s=170667a&w=0&k=20&c=YRnjpFakKMaeSnITCQcmKJ3oCYpjgo5bbJqz4BjkEjo="
                     alt="">
             </div>
             <div class="col-12 col-md-6 align-self-center ps-3  pb-3" data-aos="fade-up-left">
                 <div>
-                    <img class="mx-auto d-block" src="../images/DreamFields Logo Green-01.png" width="7%"
+                    <img class="mx-auto d-block" src="../images/DreamFields Logo Green-01.png" width="5%"
                         alt="">
-                    <h1 class="fw-bold mb-3"><span class="border-bottom border-5">Mission</span></h1>
+                    <h1 class="fw-bold mb-3 text-center py-2"><span class="border-bottom border-b">Mission</span></h1>
 
                     <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                        Our mission at <span class="fw-bold"> Dream Fields </span> is to provide our clients with the
+                        Our mission at <span class="fw-bold"> Dream <span class="orangeText">Fields</span> </span> is to provide our clients with the
                         highest
                         level of service and expertise in the
                         real estate industry. We are committed to helping our clients achieve their real estate goals by
@@ -56,17 +56,17 @@
         </div>
         <div class="row g-1 swap-column mb-3">
             <div class="col-12 col-md-6 align-self-center  ps-3  pb-3" data-aos="fade-up-right">
-                <img class="mx-auto d-block" src="../images/DreamFields Logo Green-01.png" width="7%" alt="">
-                <h1 class="fw-bold mb-3"><span class="border-bottom border-5"">Vision</span></h1>
+                <img class="mx-auto d-block" src="../images/DreamFields Logo Green-01.png" width="5%" alt="">
+                <h1 class="fw-bold mb-3 text-center py-2"><span class="border-bottom border-b">Vision</span></h1>
 
                 <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                    Our vision at <span class="fw-bold"> Dream Fields </span> is to be the leading real estate company in
+                    Our vision at <span class="fw-bold"> Dream <span class="orangeText">Fields</span> </span> is to be the leading real estate company in
                     <span class="fw-bold"> Philippines</span> and beyond, known for providing exceptional service and
                     delivering outstanding results for our clients. We strive to be a
                     trusted partner for all of our clients' real estate needs and to consistently exceed their expectations.
                 </p>
             </div>
-            <div class="col-12 col-md-6 align-self-center  pb-3" data-aos="fade-up-left">
+            <div class="col-12 col-md-6 align-self-center pb-3" data-aos="fade-up-left">
                 <img class="img-fluid mx-auto d-block shadow" style="max-width: 100%;"
                     src="https://media.istockphoto.com/id/1360521208/photo/businessman-using-a-computer-for-analysis-seo-search-engine-optimization-marketing-ranking.jpg?b=1&s=170667a&w=0&k=20&c=xyIKuSwGq65eVI8YLRIku2F8w5JkUG11E1AoSAfulDU="
                     alt="">
@@ -79,8 +79,8 @@
                     alt="">
             </div>
             <div class="col-12 col-md-6 align-self-center  ps-3  pb-3" data-aos="fade-up-left">
-                <img class="mx-auto d-block" src="../images/DreamFields Logo Green-01.png" width="7%" alt="">
-                <h1 class="fw-bold mb-3"><span class="border-bottom border-5"">Values</span></h1>
+                <img class="mx-auto d-block" src="../images/DreamFields Logo Green-01.png" width="5%" alt="">
+                <h1 class="fw-bold mb-3 text-center py-2"><span class="border-bottom border-b">Values</span></h1>
                 <ul>
                     <li>
                         <span class="fw-bold"> Customer Focus: </span> Putting the needs and interests of our clients first
