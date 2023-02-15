e8a8cf6948de91f5100e3e08df2e99907a332dc5
Chris jhovin Cabral <chrisjhovincabral25@yahoo.com>
2023-02-15 12:16:48 +0800

    update

--- resources/views/inquire.blade.php
+++ resources/views/inquire.blade.php
@@ -2,61 +2,51 @@
 
 @section('content')
 <div class="container">
-        <div class="row">
+        <div class="row py-4">
             <div class="col pt-5">
             <img class="mx-auto d-block mb-2" src="../images/DreamFields Logo Green-01.png" width="4%" alt="">
-                <p class="text-center fs-2 mt-4 mb-0"> A convenient way to inquire our property.</p>
+                <p class="text-center fs-2 my-4 mb-0"> A convenient way to inquire our property.</p>
                 
             </div>
         </div>
 
         <form>
         @csrf
-        <div class="row">
-            <div class="col-5 mt-5">
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Name*" name="name">
+        <div class=" container border border-1 border-light bg-light rounded-2 shadow">
+            <div class=" mt-5 d-flex">
+            
+                <input type="text" class="bg-primary input textcolorblack form-control bg-transparent border-0 border-bottom rounded-0 border-b" id="exampleFormControlInput1" placeholder="Name*" name="name">
+                <input type="text" class="bg-primary input textcolorblack form-control bg-transparent border-0 border-bottom rounded-0 border-b mx-4" placeholder="Contact Number*" aria-label="Contact Number" name="contact_number">
+                <input type="text" class="bg-primary input textcolorblack form-control bg-transparent border-0 border-bottom rounded-0 border-b" placeholder="Email Adress*" aria-label="Email Adress" name="e_add">
             </div>
 
-            <div class="row mt-4">
-                <div class="col-3">
-                    <input type="text" class="form-control" placeholder="Contact Number*" aria-label="Contact Number" name="contact_number">
-                </div>
-                <div class="col-4">
-                    <input type="text" class="form-control" placeholder="Email Adress*" aria-label="Email Adress" name="e_add">
-                </div>
-
-
-                <div class="row">
-                    <div class="col-14 mt-4">
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Subject*" name="subject" >
+            <div class="col-lg-12 mt-4">
+                <div class="">
+                    <div class=" mt-4">
+                        <input type="text" class="bg-primary input textcolorblack form-control bg-transparent border-0 border-bottom rounded-0 border-b" id="exampleFormControlInput1" placeholder="Subject*" name="subject" >
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col mt-4">
+                <div class="">
+                    <div class=" mt-4">
                         <div class="form-floating">
-                            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" name="message"></textarea>
+                            <textarea class="bg-primary input textcolorblack form-control bg-transparent border-0 border-bottom rounded-0 border-b" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" name="message"></textarea>
                             <label for="floatingTextarea2">Message</label>
                         </div>
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col mt-4 ml-4">
-                    <button type="submit" class="btn btn-warning"><b>Submit</b></button>
+                <div class="mb-3">
+                    <div class="col mt-4 ml-4 d-flex justify-content-end">
+                    <button type="submit" class="btn orangebutton text-light"><b>Submit</b></button>
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col mt-5">
-                        Or <br>
-                        Contact Us
-                    </div>
-                </div>
+                  
 
                
     </form>
-
-
+</div>
+ 
 
     @endsection
